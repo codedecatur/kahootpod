346ecae6377dae3851f99f3618d82564713f3072
VeryCleverName3 <awesome827740@gmail.com>
2021-01-04 15:53:09 -0500

    wss :(

--- kahootws.js
+++ kahootws.js
@@ -18,6 +18,10 @@ function kahootws(wss){
             }
         })
 
+        ws.on("open", (...args) => {
+            ws.send("hello!");
+        })
+
         ws.on("close", (...args) => {
             clients.splice(clients.indexOf(ws), 1);
         })
