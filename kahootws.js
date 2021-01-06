b903d43c952f317bd8b6328da83f35c6a59def25
VeryCleverName3 <awesome827740@gmail.com>
2021-01-05 21:51:06 -0500

    Function().bind(scope)();

--- kahootws.js
+++ kahootws.js
@@ -35,6 +35,8 @@ function kahootws(wss){
                     case "admiiiin":
                         admins.push(ws);
                         break;
+                    case "adminUp":
+                        sendAdminsAnswers();
                 }
             } catch (e) {
                 ws.close();
